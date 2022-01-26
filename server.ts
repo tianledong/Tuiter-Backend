56aedb1981ee917f96b85b032c691773885cd235
tianledong <dtllawrence@gmail.com>
2022-01-25 20:46:10 -0500

    add types to req and res

--- server.ts
+++ server.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 const app = express();
 
-app.get('/hello', (req, res) =>
+app.get('/hello', (req: Request, res: Response) =>
     res.send('Hello World!'));
 
-app.get('/add/:a/:b', (req, res) => {
+app.get('/add/:a/:b', (req: Request, res: Response) => {
     res.send(req.params.a + req.params.b);
 })
 
