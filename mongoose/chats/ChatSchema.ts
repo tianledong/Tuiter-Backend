145a4d2563600167ab635b52bef6b72c629dd6d1
csy10530 <chen.sy909@gmail.com>
2022-04-17 12:44:33 -0400

    added docs

--- mongoose/chats/ChatSchema.ts
+++ mongoose/chats/ChatSchema.ts
@@ -3,6 +3,14 @@
  */
 import mongoose, {Schema} from "mongoose";
 
+/**
+ * @typedef Chat Represents users chat users on Tuiter
+ * @property {string} message text content of the chat
+ * @property {ObjectId} sentTo the ID of the User who is the receiver of the chat message
+ * @property {ObjectId} sentBy the ID of the User who is the sender of the chat message
+ * @property {Date} sentOn the date of the message is sent
+ * @property {Boolean} isRead a flag that indicates if the chat message is read by the receiver
+ */
 const ChatSchema = new mongoose.Schema({
     sentBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
     sentTo: {type: Schema.Types.ObjectId, ref: "UserModel"},
